90a64d66446a3d792fd19fe3cab0b2af659a4b4f
Abbash Ali <abbasali6021@gmail.com>
2023-10-21 19:05:51 +0530

    chore(redux) - set up Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect } from 'react'
 import './App.css'
+import fetchData from './utils/fetchData'
+import { useDispatch, useSelector } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  const url = useSelector(state => state.home.url)
+  console.log('url', url)
+
+
+  useEffect(() => {
+    fetchApiData()
+  }, [])
+
+  const fetchApiData = () => {
+    fetchData("movie/popular")
+      .then(res => {
+        console.log(res)
+        dispatch(getApiConfiguration(res))
+      })
+      .catch(error => console.log(error))
+  }
 
   return (
     <>
-      Movie app setup
+      <p>
+        Movie app setup
+      </p>
+
     </>
   )
 }
