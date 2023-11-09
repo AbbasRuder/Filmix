f4976ad2b33ae96663fee3b2e6094bc189c1e7f9
Abbash Ali <abbasali6021@gmail.com>
2023-11-09 23:24:32 +0530

    feat(explore) - Filter movies/tv-shows

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,8 +1,11 @@
 import "./style.scss"
 import { useState, useEffect } from 'react'
+// -helpers
 import fetchDataFromApi from '../../utils/fetchData'
+import useFetch from "../../hooks/useFetch";
 // -libraries
 import { useParams } from 'react-router-dom'
+import Select from "react-select"
 import InfiniteScroll from "react-infinite-scroll-component";
 // -components
 import {
@@ -12,8 +15,18 @@ import {
 } from "../../components"
 
 
-// -filtering options
-const sortbyData = [
+// -filtering options for sortby for movies and tv-shows
+const sortByDataForTV = [
+  { value: "popularity.desc", label: "Popularity Descending" },
+  { value: "popularity.asc", label: "Popularity Ascending" },
+  { value: "vote_average.desc", label: "Rating Descending" },
+  { value: "vote_average.asc", label: "Rating Ascending" },
+  { value: "primary_release_date.desc", label: "Release Date Descending", },
+  { value: "primary_release_date.asc", label: "Release Date Ascending" },
+  { value: "original_title.asc", label: "Title (A-Z)" },
+];
+
+const sortByDataForMovies = [
   { value: "popularity.desc", label: "Popularity Descending" },
   { value: "popularity.asc", label: "Popularity Ascending" },
   { value: "vote_average.desc", label: "Rating Descending" },
@@ -32,12 +45,16 @@ export default function Explore() {
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
+  const [genre, setGenre] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
 
   const { mediaType } = useParams()
 
-  const fetchInitialData = () => {
+  const { data: genresData } = useFetch(`/genre/${mediaType}/list`);
+
+  const fetchInitialData = (params) => {
     setLoading(true)
-    fetchDataFromApi(`/discover/${mediaType}`, filters)
+    fetchDataFromApi(`/discover/${mediaType}`, params)
       .then(res => {
         setData(res)
         setPageNumber(2)
@@ -66,8 +83,37 @@ export default function Explore() {
   }
 
   useEffect(() => {
+    setGenre(null)
+    setSortBy(null)
+    // -fetching initial data with no parameters
     fetchInitialData()
   }, [mediaType])
+
+  const handleFilterSelection = (selectedItem, action) => {
+    if (action.name === "genres") {
+      setGenre(selectedItem)
+      if (action.action !== "clear") {
+        let genreIds = selectedItem.map(genre => genre.id)
+        genreIds = genreIds.join(',')
+        filters.with_genres = genreIds
+      } else {
+        delete filters.with_genres
+      }
+    }
+
+    if (action.name === "sortby") {
+      setSortBy(selectedItem)
+      if (action.action !== "clear") {
+        filters.sort_by = selectedItem.value
+      } else {
+        delete filters.sort_by
+      }
+    }
+
+    setPageNumber(1)
+    // -fetching initial data with filter parameters
+    fetchInitialData(filters)
+  }
 
   return (
     <div className="explorePage">
@@ -75,6 +121,36 @@ export default function Explore() {
         <div className="pageHeader">
           <div className="pageTitle">
             {mediaType === 'movie' ? 'Explore Movies' : 'Explore TV Shows'}
+          </div>
+
+          <div className="filters">
+            <Select
+              isMulti
+              name="genres"
+              value={genre}
+              options={genresData?.genres}
+              getOptionLabel={option => option.name}
+              getOptionValue={option => option.id}
+              onChange={handleFilterSelection}
+              placeholder="Select genres"
+              className="react-select-container genresDD"
+              classNamePrefix="react-select"
+            />
+
+            {/* notice in this Select, we are not using "getOptionLabel" and "getOptionValue", and that is because the object
+                'sortByData' that we are using, already has `label` and `value` property which is what the react-select lib 
+                looks for to use as label and value by default. */}
+            <Select
+              name="sortby"
+              value={sortBy}
+              options={mediaType === "movie" ? sortByDataForMovies : sortByDataForTV}
+              onChange={handleFilterSelection}
+              // -provides the clear icon to clear the selections. (don't have to explicitly define it in case of `multi` select but have to define it here)
+              isClearable={true}
+              placeholder="Sort by"
+              className="react-select-container sortbyDD"
+              classNamePrefix="react-select"
+            />
           </div>
         </div>
 
